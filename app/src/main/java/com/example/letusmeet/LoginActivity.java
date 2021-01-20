f8bfbfca054ef3ffac157aa9505f5746ac03b200
ShrutiRawal <shruti456rawal@gmail.com>
2021-01-20 20:01:33 +0530

    Login and Register setup

---
+++ app/src/main/java/com/example/letusmeet/LoginActivity.java
@@ -0,0 +1,128 @@
+package com.example.letusmeet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText txtMail;
+    EditText txtPass;
+    Button btnLogin, btnReg;
+    ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtMail = (EditText)findViewById(R.id.textMail);
+        txtPass = (EditText)findViewById(R.id.textPass);
+        btnLogin = (Button)findViewById(R.id.btnlogin);
+        btnReg = (Button)findViewById(R.id.btnReg);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = txtMail.getText().toString();
+                String password = txtPass.getText().toString();
+                if (validateInput(email,password)){
+                    login(email,password);
+                }
+            }
+        });
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = txtMail.getText().toString();
+                String password = txtPass.getText().toString();
+                if (validateInput(email,password)){
+                    register(email,password);
+                }
+            }
+        });
+
+    }
+
+    private boolean validateInput(String email, String password){
+        if(password == null || password.trim().length() == 0){
+            Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(email == null || email.trim().length() == 0){
+            Toast.makeText(this, "E-mail is required", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void login(String email, String password) {
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(LoginActivity.this, "Success",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this,MeetHomeActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+        });
+    }
+
+    private void addToDB(String email, String password){
+        User user = new User(email,email,password);
+        db.collection("users").document().set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(LoginActivity.this, " User registered successfully !",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void register(final String email, final String password) {
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    addToDB(email,password);
+                    //Toast.makeText(LoginActivity.this, " User registered successfully !",Toast.LENGTH_SHORT).show();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                }else{
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/letusmeet/User.java
@@ -0,0 +1,12 @@
+package com.example.letusmeet;
+
+
+public class User {
+    public String name,email,pass;
+    public User(String name,String email,String pass){
+        this.name = name;
+        this.email = email;
+        this.pass = pass;
+    }
+
+}
